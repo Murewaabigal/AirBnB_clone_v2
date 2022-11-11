e9cf4f4c039527b4f1ec87b9155663bde3a89682
murewaabigal <murewaabigal.com>
2022-11-11 01:12:21 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" This module starts a Flask web application
+"""
+     Starts a Flask web application
 listening on 0.0.0.0, port 5000
 Routes:
-    /: display “Hello HBNB!”
+    /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask
-
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
-    """Display Hello HBNB!"""
-    return "Hello HBNB!"
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0")
